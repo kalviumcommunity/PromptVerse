d9e943f71e38e98a56b08e5ad611dad9a184129a
rishi <mr.rishiganesh@gmail.com>
2023-05-22 11:43:39 +0530

    Updated server.js and created JSON files
    Created GET APIs for fetching the cardData. Also converted all the JS data files to JSON format.

--- BackEnd/Server/server.js
+++ BackEnd/Server/server.js
@@ -1,10 +1,50 @@
 const express = require("express");
+const ChatGpt = require("../../FrontEnd/src/Components/CardData/ChatGpt.json");
+const Midjourney = require("../../FrontEnd/src/Components/CardData/Midjourney.json");
+const DallE = require("../../FrontEnd/src/Components/CardData/DallE.json");
+const StableDiffusion = require("../../FrontEnd/src/Components/CardData/StableDiffusion.json");
+
+const ChatGptPath = "../../FrontEnd/src/Components/CardData/ChatGpt.json";
+const MidjourneyPath = "../../FrontEnd/src/Components/CardData/Midjourney.json";
+const DallEPath = "../../FrontEnd/src/Components/CardData/DallE.json";
+const StableDiffusionPath =
+  "../../FrontEnd/src/Components/CardData/StableDiffusion.json";
 
 const app = express();
 const port = 4002;
+app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.json("Hello, World");
+app.get("/api/prompts/chatgpt", (req, res) => {
+  res.json(ChatGpt);
+});
+
+app.get("/api/prompts/midjourney", (req, res) => {
+  res.json(Midjourney);
+});
+
+app.get("/api/prompts/stablediffusion", (req, res) => {
+  res.json(StableDiffusion);
+});
+
+app.get("/api/prompts/dalle", (req, res) => {
+  res.json(DallE);
+});
+
+app.get("/api/prompt/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const prompt = await Prompt.findById(id);
+
+    if (!prompt) {
+      return res.status(404).json({ error: "Prompt not found" });
+    }
+
+    res.json({ prompt });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Server error" });
+  }
 });
 
 app.listen(port, () => {
